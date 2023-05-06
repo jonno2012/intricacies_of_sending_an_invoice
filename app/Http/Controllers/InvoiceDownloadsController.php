a4be7986625bd4a16402971df0159baabbf2bc71
jonno2012 <jonathanrhill78@gmail.com>
2023-05-06 14:54:52 +0100

    basic pdf gen

--- app/Http/Controllers/InvoiceDownloadsController.php
+++ app/Http/Controllers/InvoiceDownloadsController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Spatie\Browsershot\Browsershot;
 
 class InvoiceDownloadsController extends Controller
 {
     public function __invoke(Request $request, $invoiceId)
     {
+
         $html = view('invoices.show', [
             'invoice' => $request->user()->subscription->invoice($invoiceId),
             'header' => 'Invoice for ' . $request->user()->name,
             'user' => $request->user()->name
-        ]);
+        ])->render();
 
-        return $html;
+        Browsershot::html($html)
+            ->showBackground()
+            ->margins(10, 10, 10, 10)
+            ->save(storage_path("app/{$invoiceId}.pdf"));
+
+        return 'done';
     }
 }
