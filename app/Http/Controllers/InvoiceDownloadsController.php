bd99eee476d8c5da12c578da1aa8d5ce208bef5b
jonno2012 <jonathanrhill78@gmail.com>
2023-05-06 15:02:22 +0100

    added a notification

--- app/Http/Controllers/InvoiceDownloadsController.php
+++ app/Http/Controllers/InvoiceDownloadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\InvoicePaid;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Spatie\Browsershot\Browsershot;
@@ -11,16 +12,22 @@ class InvoiceDownloadsController extends Controller
     public function __invoke(Request $request, $invoiceId)
     {
 
+        Mail::raw('Melllo', fn($message) => $message->to('jrhillde@gmail.com'));
+
         $html = view('invoices.show', [
             'invoice' => $request->user()->subscription->invoice($invoiceId),
             'header' => 'Invoice for ' . $request->user()->name,
             'user' => $request->user()->name
         ])->render();
 
+        $invoicePath = storage_path("app/{$invoiceId}.pdf");
+
         Browsershot::html($html)
             ->showBackground()
             ->margins(10, 10, 10, 10)
-            ->save(storage_path("app/{$invoiceId}.pdf"));
+            ->save();
+
+        $request->user($invoicePath)->notify(new InvoicePaid($invoicePath));
 
         return 'done';
     }
